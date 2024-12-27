058a33768f643d679f43b35f6b7d626751de374d
JollyPatel25 <87369487+JollyPatel25@users.noreply.github.com>
2024-12-27 12:55:08 +0000

    Sync LeetCode submission - Contains Duplicate (java)

---
+++ my-submissions/problems/contains_duplicate/solution.java
@@ -0,0 +1,13 @@
+import java.util.Set;
+import java.util.HashSet;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
+        }
+        if(set.size() < nums.length)
+            return true;
+        return false;
+    }
+}

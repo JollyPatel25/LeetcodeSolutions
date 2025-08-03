94b8e0ce4606c2a0657cf68d51e0643de29ef31b
JollyPatel25 <87369487+JollyPatel25@users.noreply.github.com>
2025-08-03 09:52:09 +0000

    Sync LeetCode submission - Reverse Linked List (java)

--- my-submissions/problems/reverse_linked_list/solution.java
+++ my-submissions/problems/reverse_linked_list/solution.java
@@ -1,16 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head == null || head.next == null)
-            return head;
-        ListNode temp = head.next, list = head, temp2 = head;
-        list.next = null;
+        ListNode prev = null;
+        ListNode newHead = null, temp = head;
         while(temp != null)
         {
-            list = temp;
-            temp = temp.next;  
-            list.next = head;
-            head = list;
+            ListNode next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
         }
-        return head;
+        return prev;
     }
 }

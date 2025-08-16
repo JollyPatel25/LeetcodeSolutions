257eaa7606a8e351e014527c59b3b817e64ef9de
JollyPatel25 <87369487+JollyPatel25@users.noreply.github.com>
2025-08-16 13:04:59 +0000

    Sync LeetCode submission - Kids With the Greatest Number of Candies (java)

--- my-submissions/problems/kids_with_the_greatest_number_of_candies/solution.java
+++ my-submissions/problems/kids_with_the_greatest_number_of_candies/solution.java
@@ -1,19 +1,17 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        List<Boolean> result = new ArrayList<Boolean>();
-        int max = -1;
-        for(int i = 0; i < candies.length; i++)
+        int max = candies[0];
+        for(int i = 1; i < candies.length; i++)
         {
-            if(max < candies[i])
+            if(candies[i] > max)
                 max = candies[i];
         }
+        List<Boolean> list = new ArrayList<Boolean>();
         for(int i = 0; i < candies.length; i++)
         {
-            if(candies[i] + extraCandies >= max)
-                result.add(true);
-            else
-                result.add(false);
+            if(candies[i] + extraCandies >= max) list.add(true);
+            else list.add(false);
         }
-        return result;
+        return list;
     }
 }

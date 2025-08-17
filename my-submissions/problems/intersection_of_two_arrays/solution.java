6d38a4b2ef78fa39148dc46d34e3492fb88e44c7
JollyPatel25 <87369487+JollyPatel25@users.noreply.github.com>
2025-08-17 12:51:16 +0000

    Sync LeetCode submission - Intersection of Two Arrays (java)

--- my-submissions/problems/intersection_of_two_arrays/solution.java
+++ my-submissions/problems/intersection_of_two_arrays/solution.java
@@ -1,25 +1,16 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        int min = nums1.length > nums2.length ? nums2.length : nums1.length;
-        int result_t[] = new int[min];
-        boolean temp[] = new boolean[1001];
-        int k = -1;
-        for(int  i = 0; i < nums1.length; i++)
-        {
-            for(int j = 0; j < nums2.length; j++)
-            {
-                if(nums1[i] == nums2[j])
-                {
-                    if(temp[nums1[i]] == false){
-                        temp[nums1[i]] = true;
-                        result_t[++k] = nums1[i];
-                    }
-                }
-            }
+        HashSet<Integer> set = new HashSet<>();
+        HashSet<Integer> result = new HashSet<>();
+        
+        for (int n : nums1) set.add(n);
+        for (int n : nums2) {
+            if (set.contains(n)) result.add(n);
         }
-        int result[] = new int[k + 1];
-        for(int i = 0 ; i <= k; i++)
-            result[i] = result_t[i];
-        return result;
+        
+        int[] res = new int[result.size()];
+        int i = 0;
+        for (int n : result) res[i++] = n;
+        return res;
     }
 }

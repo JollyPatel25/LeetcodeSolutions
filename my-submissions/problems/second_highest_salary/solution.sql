cac2ef872bc503e9960d455cbafe655112f6be2e
JollyPatel25 <87369487+JollyPatel25@users.noreply.github.com>
2025-11-08 06:01:34 +0000

    Sync LeetCode submission - Second Highest Salary (mysql)

---
+++ my-submissions/problems/second_highest_salary/solution.sql
@@ -0,0 +1,8 @@
+SELECT COALESCE((
+            SELECT DISTINCT SALARY 
+            FROM EMPLOYEE ORDER BY SALARY DESC
+            LIMIT 1 OFFSET 1
+        ), 
+        NULL
+    ) 
+AS "SecondHighestSalary";

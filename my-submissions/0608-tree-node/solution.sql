ccdfa7c94f3e4848ad55dee551fc6c14849f5700
JollyPatel25 <87369487+JollyPatel25@users.noreply.github.com>
2025-11-22 09:46:09 +0000

    Sync LeetCode submission Runtime - 198 ms (94.26%), Memory - 0.0B (100.00%)

---
+++ my-submissions/0608-tree-node/solution.sql
@@ -0,0 +1,9 @@
+-- Write your PostgreSQL query statement below
+SELECT ID, (CASE WHEN P_ID IS NULL THEN 'Root' 
+                WHEN ID IN (
+                        SELECT DISTINCT P_ID FROM TREE
+                    ) THEN 'Inner' 
+                ELSE 'Leaf' 
+            END)
+AS TYPE
+FROM TREE;

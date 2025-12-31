80bd70b9c790b352e7106106cf907b55443987cf
JollyPatel25 <87369487+JollyPatel25@users.noreply.github.com>
2025-12-31 16:33:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.9 MB (50.00%)

---
+++ my-submissions/2107-find-unique-binary-string/solution.cs
@@ -0,0 +1,10 @@
+public class Solution {
+    public string FindDifferentBinaryString(string[] nums) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < nums.Length; i++) {
+            sb.Append(nums[i][i] == '0' ? '1' : '0');
+        }
+        return sb.ToString();
+    }
+}
+

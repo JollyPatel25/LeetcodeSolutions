3b75771dbe6cbc5a04f6b1e2b84a2163d58a98a5
JollyPatel25 <87369487+JollyPatel25@users.noreply.github.com>
2026-01-14 02:47:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 29 MB (45.24%)

---
+++ my-submissions/2323-minimum-bit-flips-to-convert-number/solution.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int MinBitFlips(int start, int goal) {
+        int count = 0;
+        while(goal > 0 || start > 0)
+        {
+            if((start & 1) != (goal & 1)) count++;
+            start >>= 1;
+            goal >>= 1;
+        }
+        return count;
+    }
+}

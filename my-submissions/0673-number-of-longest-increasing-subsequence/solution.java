e886841bc380f185430ad4a5e5cdcd6ffffd97e8
JollyPatel25 <87369487+JollyPatel25@users.noreply.github.com>
2026-06-06 14:08:19 +0000

    Sync LeetCode submission Runtime - 26 ms (9.50%), Memory - 45.7 MB (24.62%)

---
+++ my-submissions/0673-number-of-longest-increasing-subsequence/solution.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int findNumberOfLIS(int[] nums) {
+
+        int[] dp = new int[nums.length];
+        int[] count = new int[nums.length];
+        Arrays.fill(dp, 1);
+        Arrays.fill(count, 1);
+
+        for (int i = 1; i < nums.length; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if(nums[j] < nums[i])
+                {
+                    if(dp[i] == dp[j] + 1)
+                    {
+                        count[i] += count[j];
+                    }
+                    else if (dp[i] < dp[j] + 1)
+                    {
+                        dp[i] = dp[j] + 1;
+                        count[i] = count[j];
+                    }
+                }
+            }
+        }
+
+        int max = Arrays.stream(dp).max().getAsInt();
+        int result = 0;
+
+        for (int i = 0; i < nums.length; i++)
+        {
+            if(dp[i] == max) result += count[i];
+        }
+
+        return result;
+    }
+}

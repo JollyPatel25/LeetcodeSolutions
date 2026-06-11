fedea2cac4d08fc1201da06e9a2a230097fd1f31
JollyPatel25 <87369487+JollyPatel25@users.noreply.github.com>
2026-06-11 18:51:55 +0000

    Sync LeetCode submission Runtime - 7 ms (23.16%), Memory - 46.6 MB (19.40%)

---
+++ my-submissions/0909-stone-game/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        int[][] dp = new int[piles.length][piles.length];
+        for (int i = 0; i < piles.length; i++) dp[i][i] = piles[i];
+        for(int len = 2; len <= piles.length; len++)
+        {
+            for (int i = 0; i <= piles.length - len; i++)
+            {
+                int j = i + len - 1;
+                dp[i][j] = Math.max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
+            }
+        }
+        return dp[0][piles.length - 1] > 0;
+    }
+}

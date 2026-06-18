85fe62c26ab385b4363e891337ae0eb5a2b4e654
JollyPatel25 <87369487+JollyPatel25@users.noreply.github.com>
2026-06-18 18:30:05 +0000

    Sync LeetCode submission Runtime - 9 ms (25.95%), Memory - 50.8 MB (5.24%)

---
+++ my-submissions/0303-range-sum-query---immutable/solution.java
@@ -0,0 +1,19 @@
+class NumArray {
+    int[] prefixSum = new int[100001];
+    public NumArray(int[] nums) {
+        for (int i = 0 ; i < nums.length; i++)
+        {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+    }
+    
+    public int sumRange(int left, int right) {
+        return prefixSum[right + 1] - prefixSum[left];
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
